dacc791619f5b18c0c75af7ce4358b017ead6458
Aodh O'Sullivan <aodhsully@gmail.com>
2017-09-04 16:50:37 +0100

    edit tracks page

---
+++ WebsiteAssignmentNew/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteAssignmentNew.Models
+{
+    public class Artist
+    {
+        public string ArtistName { get; set; }
+        public string Title { get; set; }
+    }
+}
